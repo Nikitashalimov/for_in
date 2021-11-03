5bf1a1da99881d30c0ddac1d11fca490f263e882
Nikita Shalimov <nikitashalimov@mail.ru>
2021-11-03 20:47:14 +0300

    с ошибкой

--- src/index.js
+++ src/index.js
@@ -1,20 +1,20 @@
 export default function orderByProps(object, arrStart) { // функция для создания массива
-	const arrKey = []; // пустой массив для ключей
-	for (const el in object) { // перебираем ключи и добавляем в массив c ключами
-		arrKey.push(el);
-	}
-	const arrResult = []; // пустой итоговый массив
-	arrKey.sort(); // сортируем массив с ключами
-	// далее пробегаемся по стартовому массиву и выносим вперед нужные элементы
-	const arrReverse = arrStart.reverse(); // переворачиваем массив из условия
-	for (let i = 0; i < arrReverse.length; i += 1) { // пробегаем по перевернутому массиву из условия
-		const index = arrKey.findIndex((item) => item === arrReverse[i]); // ищем индекс элементов
-		arrKey.unshift(...arrKey.splice(index, 1)); // вырезаем и вставляем в начало массива с ключами
-	}
-	for (let i = 0; i < arrKey.length; i += 1) { // циклом пробегаемся по массиву с ключами
-		const k = arrKey[i];
-		const newObject = { key: k, value: object[k] };
-		arrResult.push(newObject); // наполняем конечный массив
-	}
-	return arrResult;
+  const arrKey = []; // пустой массив для ключей
+  for (const el in object) { // перебираем ключи и добавляем в массив c ключами
+    arrKey.push(el);
+  }
+  const arrResult = []; // пустой итоговый массив
+  arrKey.sort(); // сортируем массив с ключами
+  // далее пробегаемся по стартовому массиву и выносим вперед нужные элементы
+  const arrReverse = arrStart.reverse(); // переворачиваем массив из условия
+  for (let i = 0; i < arrReverse.length; i += 1) { // пробегаем по перевернутому массиву из условия
+    const index = arrKey.findIndex((item) => item === arrReverse[i]); // ищем индекс элементов
+    arrKey.unshift(...arrKey.splice(index, 1)); // вырезаем и вставляем в начало массива с ключами
+  }
+  for (let i = 0; i < arrKey.length; i += 1) { // циклом пробегаемся по массиву с ключами
+    const k = arrKey[i];
+    const newObject = { key: k, value: object[k] };
+    arrResult.push(newObject); // наполняем конечный массив
+  }
+  return arrResult;
 }
